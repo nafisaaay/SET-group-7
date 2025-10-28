a370b8e9482b6d7e659011a9bc2763a8cfb2e2d7
Aaro <aarois@hiof.no>
2025-10-28 09:22:48 +0100

    Merge remote-tracking branch 'origin/main'

--- backend/src/main/java/no/hiof/setgroup7/DTOs/SQLdbConnect.java
+++ backend/src/main/java/no/hiof/setgroup7/DTOs/SQLdbConnect.java
@@ -1,13 +1,14 @@
 package no.hiof.setgroup7.DTOs;
 import java.sql.*;
 
+//this function only works in all likelihood when connected to the school VPN or when connected to school internet
 
 public class SQLdbConnect {
 
     public void SQLdbConnectMethod() {
 
 
-        String url = "jdbc:mysql://itstud.hiof.no:3306/se25_G7"; // The url for the database, should adjust it for actual one
+        String url = "jdbc:mysql://itstud.hiof.no:3306/se25_G7"; // Url, username and password should be instead fetched using <object>.getUrl, <object>.getUsername, <object>.getPassword,
         String username = "gruppe7"; //username should be abstracted
         String password = "Summer26"; //should be abstracted again
         Connection connection = null;
@@ -17,10 +18,12 @@ public class SQLdbConnect {
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Successful connection to the database.");
 
-
+            /* tested successfully the following query to see if the connection worked
             String createTableSQL = "CREATE TABLE Car ("
                     + "brand VARCHAR(50), "
                     + "colour VARCHAR(50))";
+
+             */
 
             Statement statement = connection.createStatement();
             statement.execute(createTableSQL);
