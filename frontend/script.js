6e2981cf1bccf3c9a5511e814cbf3234b194092a
Abdis10 <abdir8198@gmail.com>
2025-11-04 10:03:57 +0100

    fix: endret portnummer i fetch slik at den matcher portnummer til javalin server

--- frontend/script.js
+++ frontend/script.js
@@ -172,7 +172,7 @@ form.addEventListener("submit", async (e) => {
     console.log(formData);
 
     try {
-        const response = await fetch("http://localhost:5000/api/trip", {
+        const response = await fetch("http://localhost:8000/api/trip", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData)
