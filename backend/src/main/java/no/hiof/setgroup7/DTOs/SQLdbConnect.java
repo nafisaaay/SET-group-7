f1018139aafdcfa9517a1722381457d444b6f43b
Baarara <aarois@hiof.no>
2025-11-14 19:02:22 +0100

    feat: filtering the PoI information to suitable format. Merge remote-tracking branch 'origin/main'

--- backend/src/main/java/no/hiof/setgroup7/DTOs/SQLdbConnect.java
+++ backend/src/main/java/no/hiof/setgroup7/DTOs/SQLdbConnect.java
@@ -20,23 +20,23 @@ public class SQLdbConnect {
 
     public void run(){
 
-        ArrayList<PoiDTO> poiFredrikstad = (ArrayList<PoiDTO>) sqlConnector.getAllPois(SQLGetFredrikstad);
-        ArrayList<PoiDTO> poiSarpsborg = (ArrayList<PoiDTO>) sqlConnector.getAllPois(SQLGetSarpsborg);
-        ArrayList<PoiDTO> poiMoss = (ArrayList<PoiDTO>) sqlConnector.getAllPois(SQLGetMoss);
-        ArrayList<PoiDTO> poiHalden = (ArrayList<PoiDTO>) sqlConnector.getAllPois(SQLGetHalden);
-        ArrayList<PoiDTO> poiOslo = (ArrayList<PoiDTO>) sqlConnector.getAllPois(SQLGetOslo);
+        ArrayList<PoiDTO> poiFredrikstad = sqlConnector.getAllPois(SQLGetFredrikstad);
+        ArrayList<PoiDTO> poiSarpsborg = sqlConnector.getAllPois(SQLGetSarpsborg);
+        ArrayList<PoiDTO> poiMoss = sqlConnector.getAllPois(SQLGetMoss);
+        ArrayList<PoiDTO> poiHalden = sqlConnector.getAllPois(SQLGetHalden);
+        ArrayList<PoiDTO> poiOslo = sqlConnector.getAllPois(SQLGetOslo);
 
-        ArrayList<PoiDTO> SarpFoodPoI = poiFilter(poiSarpsborg, "café", "restaurant", "tavern");;
-        ArrayList<PoiDTO> FredFoodPoI = poiFilter(poiFredrikstad, "café", "restaurant", "tavern");
-        ArrayList<PoiDTO> MossFoodPoI = poiFilter(poiMoss, "café", "restaurant", "tavern");
-        ArrayList<PoiDTO> HaldenFoodPoI = poiFilter(poiHalden, "café", "restaurant", "tavern");
-        ArrayList<PoiDTO> OsloFoodPoI = poiFilter(poiOslo, "café", "restaurant", "tavern");
+        ArrayList SarpFoodPoI = poiFilter(poiSarpsborg, "café", "restaurant", "tavern");
+        ArrayList FredFoodPoI = poiFilter(poiFredrikstad, "café", "restaurant", "tavern");
+        ArrayList MossFoodPoI = poiFilter(poiMoss, "café", "restaurant", "tavern");
+        ArrayList HaldenFoodPoI = poiFilter(poiHalden, "café", "restaurant", "tavern");
+        ArrayList OsloFoodPoI = poiFilter(poiOslo, "café", "restaurant", "tavern");
 
-        ArrayList<PoiDTO> SarpCulturePoI = poiFilter(poiSarpsborg, "museum", "art gallery", "*landmark");
-        ArrayList<PoiDTO> FredCulturePoI = poiFilter(poiFredrikstad, "museum", "art gallery", "*landmark");
-        ArrayList<PoiDTO> MossCulturePoI = poiFilter(poiMoss, "museum", "art gallery", "*landmark");
-        ArrayList<PoiDTO> HaldenCulturePoi = poiFilter(poiHalden, "museum", "art gallery", "*landmark");
-        ArrayList<PoiDTO> OsloCulturePoI = poiFilter(poiOslo, "museum", "art gallery", "*landmark");
+        ArrayList SarpCulturePoI = poiFilter(poiSarpsborg, "museum", "art gallery", "*landmark");
+        ArrayList FredCulturePoI = poiFilter(poiFredrikstad, "museum", "art gallery", "*landmark");
+        ArrayList MossCulturePoI = poiFilter(poiMoss, "museum", "art gallery", "*landmark");
+        ArrayList HaldenCulturePoi = poiFilter(poiHalden, "museum", "art gallery", "*landmark");
+        ArrayList OsloCulturePoI = poiFilter(poiOslo, "museum", "art gallery", "*landmark");
 
 
     }
