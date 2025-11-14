b3871e03b7884fff543daaf00c87a66221e9c627
Baarara <aarois@hiof.no>
2025-11-14 19:00:21 +0100

    feat: filtering the PoI information to suitable format. Merge remote-tracking branch 'origin/main'

--- backend/src/main/java/no/hiof/setgroup7/model/PoIFilter.java
+++ backend/src/main/java/no/hiof/setgroup7/model/PoIFilter.java
@@ -10,7 +10,7 @@ public class PoIFilter {
     public static ArrayList poiFilter(ArrayList<PoiDTO> fromList,  String filterCondition1, String filterCondition2, String filterCondition3) {
         ArrayList<PoiDTO> toList = new ArrayList<>();
         for (PoiDTO pointOfInterest : fromList) {
-            if (pointOfInterest.getPlaceType() == filterCondition1 || pointOfInterest.getPlaceType() == filterCondition2 || pointOfInterest.getPlaceType() == filterCondition3) {
+            if (pointOfInterest.getPlaceType().equals(filterCondition1) || pointOfInterest.getPlaceType().equals(filterCondition2) || pointOfInterest.getPlaceType().equals(filterCondition3)) {
                 toList.add(pointOfInterest);
             }
         }
